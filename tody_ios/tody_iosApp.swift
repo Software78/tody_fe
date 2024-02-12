8368356e6ca483e3ca363e058eedb94b18f64aa6
Owen <technicatalyst78@gmail.com>
2024-02-12 06:36:56 +0100

    Initial Commit

---
+++ tody_ios/tody_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tody_iosApp.swift
+//  tody_ios
+//
+//  Created by Adedola Owen Abaru on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct tody_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
